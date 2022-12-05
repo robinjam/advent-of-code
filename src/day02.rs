5ce42e2282af2f3ec99c19f1191fb2ddf3f68ac5
James Robinson <james@robinjam.net>
2022-12-05 23:57:37 +0000

    Refactor

--- src/day02.rs
+++ src/day02.rs
@@ -7,6 +7,18 @@ enum Move {
     Scissors,
 }
 
+#[derive(Debug, PartialEq)]
+struct Round {
+    their_move: Move,
+    my_move: Move,
+}
+
+enum Result {
+    Lose,
+    Draw,
+    Win,
+}
+
 fn move_from_char(c: char) -> Move {
     match c {
         'A' => Move::Rock,
@@ -19,39 +31,45 @@ fn move_from_char(c: char) -> Move {
     }
 }
 
-fn moves_from_line(line: String) -> (Move, Move) {
+fn parse_round(line: String) -> Round {
     let move_from_nth_char = |n| move_from_char(line.chars().nth(n).unwrap());
-    (move_from_nth_char(0), move_from_nth_char(2))
+    Round{ their_move: move_from_nth_char(0), my_move: move_from_nth_char(2) }
 }
 
 #[test]
-fn test_moves_from_line() {
-    assert_eq!((Move::Rock, Move::Paper), moves_from_line("A Y".into()));
-    assert_eq!((Move::Paper, Move::Rock), moves_from_line("B X".into()));
-    assert_eq!((Move::Scissors, Move::Scissors), moves_from_line("C Z".into()));
+fn test_parse_round() {
+    assert_eq!(Round{ their_move: Move::Rock, my_move: Move::Paper }, parse_round("A Y".into()));
+    assert_eq!(Round{ their_move: Move::Paper, my_move: Move::Rock }, parse_round("B X".into()));
+    assert_eq!(Round{ their_move: Move::Scissors, my_move: Move::Scissors }, parse_round("C Z".into()));
 }
 
-fn score(moves: (Move, Move)) -> i32 {
-    let (theirs, mine) = moves;
+fn result(round: Round) -> Result {
+    match (round.my_move, round.their_move) {
+        (Move::Rock, Move::Scissors) => Result::Win,
+        (Move::Paper, Move::Rock) => Result::Win,
+        (Move::Scissors, Move::Paper) => Result::Win,
 
-    let shape_score = match mine {
+        (Move::Rock, Move::Rock) => Result::Draw,
+        (Move::Paper, Move::Paper) => Result::Draw,
+        (Move::Scissors, Move::Scissors) => Result::Draw,
+
+        (Move::Rock, Move::Paper) => Result::Lose,
+        (Move::Paper, Move::Scissors) => Result::Lose,
+        (Move::Scissors, Move::Rock) => Result::Lose,
+    }
+}
+
+fn score(round: Round) -> i32 {
+    let shape_score = match round.my_move {
         Move::Rock => 1,
         Move::Paper => 2,
         Move::Scissors => 3
     };
 
-    let outcome_score = match (mine, theirs) {
-        (Move::Rock, Move::Scissors) => 6,
-        (Move::Paper, Move::Rock) => 6,
-        (Move::Scissors, Move::Paper) => 6,
-
-        (Move::Rock, Move::Rock) => 3,
-        (Move::Paper, Move::Paper) => 3,
-        (Move::Scissors, Move::Scissors) => 3,
-
-        (Move::Rock, Move::Paper) => 0,
-        (Move::Paper, Move::Scissors) => 0,
-        (Move::Scissors, Move::Rock) => 0,
+    let outcome_score = match result(round) {
+        Result::Lose => 0,
+        Result::Draw => 3,
+        Result::Win => 6
     };
 
     shape_score + outcome_score
@@ -59,15 +77,15 @@ fn score(moves: (Move, Move)) -> i32 {
 
 #[test]
 fn test_score() {
-    assert_eq!(8, score((Move::Rock, Move::Paper)));
-    assert_eq!(1, score((Move::Paper, Move::Rock)));
-    assert_eq!(6, score((Move::Scissors, Move::Scissors)));
+    assert_eq!(8, score(Round{ their_move: Move::Rock, my_move: Move::Paper }));
+    assert_eq!(1, score(Round{ their_move: Move::Paper, my_move: Move::Rock }));
+    assert_eq!(6, score(Round{ their_move: Move::Scissors, my_move: Move::Scissors }));
 }
 
 pub fn run() {
     let file = File::open("data/02.txt").unwrap();
     let lines = BufReader::new(file).lines().map(|line| line.unwrap());
-    let score: i32 = lines.map(moves_from_line).map(score).sum();
+    let score: i32 = lines.map(parse_round).map(score).sum();
 
     println!("Day 02");
     println!("  Part 1: {}", score);
