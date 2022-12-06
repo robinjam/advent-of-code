34b1c5af9b76f2aa214bfc554a1adb8d57625107
James Robinson <james@robinjam.net>
2022-12-06 00:20:40 +0000

    Refactor

--- src/day02.rs
+++ src/day02.rs
@@ -46,27 +46,13 @@ fn parse_round(line: String) -> Round {
     Round{ their_move: move_from_nth_char(0), my_move: move_from_nth_char(2) }
 }
 
-#[test]
-fn test_parse_round() {
-    assert_eq!(Round{ their_move: Move::Rock, my_move: Move::Paper }, parse_round("A Y".into()));
-    assert_eq!(Round{ their_move: Move::Paper, my_move: Move::Rock }, parse_round("B X".into()));
-    assert_eq!(Round{ their_move: Move::Scissors, my_move: Move::Scissors }, parse_round("C Z".into()));
-}
-
 fn parse_move_and_result(line: String) -> (Move, Result) {
     let nth_char = |n| line.chars().nth(n).unwrap();
     (move_from_char(nth_char(0)), result_from_char(nth_char(2)))
 }
 
-#[test]
-fn test_parse_move_and_result() {
-    assert_eq!((Move::Rock, Result::Draw), parse_move_and_result("A Y".into()));
-    assert_eq!((Move::Paper, Result::Lose), parse_move_and_result("B X".into()));
-    assert_eq!((Move::Scissors, Result::Win), parse_move_and_result("C Z".into()));
-}
-
-fn choose_strategy(their_move: Move, desired_result: Result) -> Round {
-    let my_move = match (&their_move, &desired_result) {
+fn choose_strategy((their_move, desired_result): (Move, Result)) -> Round {
+    let my_move = match (&their_move, desired_result) {
         (Move::Rock, Result::Win) => Move::Paper,
         (Move::Paper, Result::Win) => Move::Scissors,
         (Move::Scissors, Result::Win) => Move::Rock,
@@ -81,13 +67,6 @@ fn choose_strategy(their_move: Move, desired_result: Result) -> Round {
     };
 
     Round{ their_move, my_move }
-}
-
-#[test]
-fn test_choose_strategy() {
-    assert_eq!(Round{ their_move: Move::Rock, my_move: Move::Rock }, choose_strategy(Move::Rock, Result::Draw));
-    assert_eq!(Round{ their_move: Move::Paper, my_move: Move::Rock }, choose_strategy(Move::Paper, Result::Lose));
-    assert_eq!(Round{ their_move: Move::Scissors, my_move: Move::Rock }, choose_strategy(Move::Scissors, Result::Win));
 }
 
 fn result(round: Round) -> Result {
@@ -122,20 +101,46 @@ fn score(round: Round) -> i32 {
     shape_score + outcome_score
 }
 
-#[test]
-fn test_score() {
-    assert_eq!(8, score(Round{ their_move: Move::Rock, my_move: Move::Paper }));
-    assert_eq!(1, score(Round{ their_move: Move::Paper, my_move: Move::Rock }));
-    assert_eq!(6, score(Round{ their_move: Move::Scissors, my_move: Move::Scissors }));
-}
-
 pub fn run() {
     let read_lines = || BufReader::new(File::open("data/02.txt").unwrap()).lines().map(|line| line.unwrap());
 
     let part_1_score: i32 = read_lines().map(parse_round).map(score).sum();
-    let part_2_score: i32 = read_lines().map(parse_move_and_result).map(|(their_move, desired_result)| choose_strategy(their_move, desired_result)).map(score).sum();
+    let part_2_score: i32 = read_lines().map(parse_move_and_result).map(choose_strategy).map(score).sum();
 
     println!("Day 02");
     println!("  Part 1: {}", part_1_score);
     println!("  Part 2: {}", part_2_score);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_round() {
+        assert_eq!(Round{ their_move: Move::Rock, my_move: Move::Paper }, parse_round("A Y".into()));
+        assert_eq!(Round{ their_move: Move::Paper, my_move: Move::Rock }, parse_round("B X".into()));
+        assert_eq!(Round{ their_move: Move::Scissors, my_move: Move::Scissors }, parse_round("C Z".into()));
+    }
+
+    #[test]
+    fn test_parse_move_and_result() {
+        assert_eq!((Move::Rock, Result::Draw), parse_move_and_result("A Y".into()));
+        assert_eq!((Move::Paper, Result::Lose), parse_move_and_result("B X".into()));
+        assert_eq!((Move::Scissors, Result::Win), parse_move_and_result("C Z".into()));
+    }
+
+    #[test]
+    fn test_choose_strategy() {
+        assert_eq!(Round{ their_move: Move::Rock, my_move: Move::Rock }, choose_strategy((Move::Rock, Result::Draw)));
+        assert_eq!(Round{ their_move: Move::Paper, my_move: Move::Rock }, choose_strategy((Move::Paper, Result::Lose)));
+        assert_eq!(Round{ their_move: Move::Scissors, my_move: Move::Rock }, choose_strategy((Move::Scissors, Result::Win)));
+    }
+
+    #[test]
+    fn test_score() {
+        assert_eq!(8, score(Round{ their_move: Move::Rock, my_move: Move::Paper }));
+        assert_eq!(1, score(Round{ their_move: Move::Paper, my_move: Move::Rock }));
+        assert_eq!(6, score(Round{ their_move: Move::Scissors, my_move: Move::Scissors }));
+    }
 }
