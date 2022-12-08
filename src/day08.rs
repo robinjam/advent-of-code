7debec72194db6f9768e8666cc8af6b63d831c1f
James Robinson <james@robinjam.net>
2022-12-08 22:12:21 +0000

    Day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -12,6 +12,43 @@ fn is_visible(trees: &Array2D<char>, x: usize, y: usize) -> bool {
     (y + 1..yn).all(|i| trees[(x, i)] < tree)
 }
 
+fn scenic_score(trees: &Array2D<char>, x: usize, y: usize) -> i32 {
+    let xn = trees.column_len();
+    let yn = trees.row_len();
+    let tree = trees[(x, y)];
+    let mut up = 0;
+    let mut down = 0;
+    let mut left = 0;
+    let mut right = 0;
+
+    for i in (0..x).rev() {
+        left += 1;
+        if trees[(i, y)] >= tree {
+            break;
+        }
+    }
+    for i in x + 1..xn {
+        right += 1;
+        if trees[(i, y)] >= tree {
+            break;
+        }
+    }
+    for i in (0..y).rev() {
+        up += 1;
+        if trees[(x, i)] >= tree {
+            break;
+        }
+    }
+    for i in y + 1..yn {
+        down += 1;
+        if trees[(x, i)] >= tree {
+            break;
+        }
+    }
+
+    up * down * left * right
+}
+
 pub fn run() -> (String, String) {
     let rows: Vec<Vec<char>> = read_to_string("data/08.txt").
         unwrap().
@@ -20,7 +57,7 @@ pub fn run() -> (String, String) {
         collect();
 
     let trees = Array2D::from_rows(rows.as_slice());
-    
+
     let mut part1 = 0;
     for x in 0..trees.column_len() {
         for y in 0..trees.row_len() {
@@ -30,5 +67,13 @@ pub fn run() -> (String, String) {
         }
     }
 
-    (part1.to_string(), "".into())
+    let mut scores: Vec<i32> = vec![];
+    for x in 0..trees.column_len() {
+        for y in 0..trees.row_len() {
+            scores.push(scenic_score(&trees, x, y));
+        }
+    }
+    let part2 = scores.iter().max().unwrap();
+
+    (part1.to_string(), part2.to_string())
 }
