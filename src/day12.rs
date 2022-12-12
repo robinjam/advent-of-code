a758b0f891c3ff4de903b4a6933f3d89c8d03c21
James Robinson <james@robinjam.net>
2022-12-12 21:36:10 +0000

    Run rust formatter

--- src/day12.rs
+++ src/day12.rs
@@ -1,17 +1,20 @@
-use std::{fs::read_to_string, str::FromStr};
-
-use anyhow::{Result, anyhow, Context, Error};
+use anyhow::{anyhow, Context, Error, Result};
 use array2d::Array2D;
 use pathfinding::prelude::dijkstra;
+use std::{fs::read_to_string, str::FromStr};
 
 pub fn run() -> Result<(String, String)> {
-    let InitialState{ heights, start, end } = read_to_string("data/12.txt")?.parse()?;
+    let InitialState {
+        heights,
+        start,
+        end,
+    } = read_to_string("data/12.txt")?.parse()?;
 
-    let part1 = steps_required(&heights, end, |&pos| { pos == start }).
-        context("part 1: can't find path to goal!")?;
+    let part1 = steps_required(&heights, end, |&pos| pos == start)
+        .context("part 1: can't find path to goal!")?;
 
-    let part2 = steps_required(&heights, end, |&pos| { heights[pos] == 0 }).
-        context("part 2: can't find path to goal!")?;
+    let part2 = steps_required(&heights, end, |&pos| heights[pos] == 0)
+        .context("part 2: can't find path to goal!")?;
 
     Ok((part1.to_string(), part2.to_string()))
 }
@@ -32,56 +35,70 @@ impl FromStr for InitialState {
         let mut start: Option<Pos> = None;
         let mut end: Option<Pos> = None;
 
-        let rows: Vec<Vec<i32>> = buf.lines().enumerate().map(|(row_idx, line)|
-            line.chars().enumerate().map(|(col_idx, char)| {
-                match char {
-                    'S' => {
-                        match start {
+        let rows: Vec<Vec<i32>> = buf
+            .lines()
+            .enumerate()
+            .map(|(row_idx, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(col_idx, char)| match char {
+                        'S' => match start {
                             Some(..) => Err(anyhow!("multiple start squares found in input")),
                             None => {
                                 start = Some((row_idx, col_idx));
                                 Ok(0)
                             }
-                        }
-                    },
-                    'E' => {
-                        match end {
+                        },
+                        'E' => match end {
                             Some(..) => Err(anyhow!("multiple end squares found in input")),
                             None => {
                                 end = Some((row_idx, col_idx));
                                 Ok(25)
                             }
-                        }
-                    },
-                    'a'..='z' => Ok(char as i32 - 'a' as i32),
-                    _ => Err(anyhow!("invalid square: {}", char))
-                }
-            }).collect()
-        ).collect::<Result<_>>()?;
+                        },
+                        'a'..='z' => Ok(char as i32 - 'a' as i32),
+                        _ => Err(anyhow!("invalid square: {}", char)),
+                    })
+                    .collect()
+            })
+            .collect::<Result<_>>()?;
 
         let heights = Array2D::from_rows(&rows);
         let start = start.context("can't find start square in input")?;
         let end = end.context("can't find end square in input")?;
 
-        Ok(InitialState{ heights, start, end })
+        Ok(InitialState {
+            heights,
+            start,
+            end,
+        })
     }
 }
 
 fn steps_required<F>(heights: &Heights, start: Pos, is_goal: F) -> Option<usize>
-    where F: Fn(&Pos) -> bool
+where
+    F: Fn(&Pos) -> bool,
 {
     let (_, steps) = dijkstra(
         &start,
         |&pos| {
             let mut neighbours = vec![];
-            if pos.0 > 0                         { neighbours.push(((pos.0 - 1, pos.1    ), 1)); }
-            if pos.1 > 0                         { neighbours.push(((pos.0    , pos.1 - 1), 1)); }
-            if pos.0 < heights.num_rows() - 1    { neighbours.push(((pos.0 + 1, pos.1    ), 1)); }
-            if pos.1 < heights.num_columns() - 1 { neighbours.push(((pos.0    , pos.1 + 1), 1)); }
+            if pos.0 > 0 {
+                neighbours.push(((pos.0 - 1, pos.1), 1));
+            }
+            if pos.1 > 0 {
+                neighbours.push(((pos.0, pos.1 - 1), 1));
+            }
+            if pos.0 < heights.num_rows() - 1 {
+                neighbours.push(((pos.0 + 1, pos.1), 1));
+            }
+            if pos.1 < heights.num_columns() - 1 {
+                neighbours.push(((pos.0, pos.1 + 1), 1));
+            }
             neighbours.retain(|next| heights[pos] - heights[next.0] <= 1);
             neighbours
         },
-        is_goal
+        is_goal,
     )?;
 
     Some(steps)
