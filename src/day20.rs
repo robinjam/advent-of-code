247fb47e25de91ebe7b275a0c9bd6d1b765ea379
James Robinson <james@robinjam.net>
2022-12-20 18:16:01 +0000

    Day 20 part 2

--- src/day20.rs
+++ src/day20.rs
@@ -5,22 +5,25 @@ use anyhow::{Result, Context};
 pub fn run() -> Result<(String, String)> {
     let numbers: Vec<_> = read_to_string("data/20.txt")?
         .lines()
-        .map(|line| line.parse::<i32>())
+        .map(|line| line.parse::<i64>())
         .collect::<Result<_, _>>()?;
 
-    let part1 = solve(&numbers).context("no solution")?;
+    let part1 = solve(&numbers, 1, 1).context("no solution")?;
+    let part2 = solve(&numbers, 811589153, 10).context("no solution")?;
 
-    Ok((part1.to_string(), "TODO".into()))
+    Ok((part1.to_string(), part2.to_string()))
 }
 
-fn solve(numbers: &[i32]) -> Option<i32> {
-    let mut numbers: Vec<_> = numbers.iter().cloned().enumerate().collect();
+fn solve(numbers: &[i64], key: i64, rounds: usize) -> Option<i64> {
+    let mut numbers: Vec<_> = numbers.iter().map(|&n| n * key).enumerate().collect();
 
-    for i in 0..numbers.len() {
-        let pos = numbers.iter().position(|&(idx, _n)| idx == i)?;
-        let (idx, n) = numbers.remove(pos);
-        let new_idx = (pos as i32 + n).rem_euclid(numbers.len() as i32) as usize;
-        numbers.insert(new_idx, (idx, n));
+    for _ in 0..rounds {
+        for i in 0..numbers.len() {
+            let pos = numbers.iter().position(|&(idx, _n)| idx == i)?;
+            let (idx, n) = numbers.remove(pos);
+            let new_idx = (pos as i64 + n).rem_euclid(numbers.len() as i64) as usize;
+            numbers.insert(new_idx, (idx, n));
+        }
     }
 
     let idx = numbers.iter().position(|&(_idx, n)| n == 0)?;
