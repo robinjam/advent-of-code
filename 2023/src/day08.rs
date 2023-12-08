8e77dcfd3bc0e42d64d3ed7f16d5cdc01f95f233
James Robinson <james@robinjam.net>
2023-12-08 22:52:44 +0000

    Day 8 part 2

--- 2023/src/day08.rs
+++ 2023/src/day08.rs
@@ -1,49 +1,78 @@
-use std::{collections::HashMap, ops::Index, str::FromStr};
+use std::collections::HashMap;
+
+use num_integer::lcm;
 
 pub fn day08(input: &str) -> (String, String) {
     (part1(input), part2(input))
 }
 
 fn part1(input: &str) -> String {
-    let (instructions, nodes) = parse(input);
-
-    let mut current_node = "AAA";
-    for (i, instruction) in instructions.enumerate() {
-        if current_node == "ZZZ" {
-            return i.to_string();
-        }
-        let (lhs, rhs) = nodes[&current_node];
-        match instruction {
-            Instruction::Left => current_node = lhs,
-            Instruction::Right => current_node = rhs,
-        }
-    }
-
-    unreachable!()
+    Network::from(input)
+        .navigate("AAA", |node| node == "ZZZ")
+        .to_string()
 }
 
 fn part2(input: &str) -> String {
-    "".into()
+    let network = Network::from(input);
+    network
+        .nodes
+        .keys()
+        .filter(|node| node.ends_with("A"))
+        .map(|start| network.navigate(&start, |node: &str| node.ends_with("Z")))
+        .reduce(lcm)
+        .unwrap()
+        .to_string()
 }
 
-fn parse(
-    input: &str,
-) -> (
-    impl Iterator<Item = Instruction> + '_,
-    HashMap<&str, (&str, &str)>,
-) {
-    let mut lines = input.lines();
-    let instructions = lines.next().unwrap().chars().map(Instruction::from).cycle();
-    let nodes = lines
-        .skip(1)
-        .map(|line| {
-            let name = &line[0..3];
-            let lhs = &line[7..10];
-            let rhs = &line[12..15];
-            (name, (lhs, rhs))
-        })
-        .collect();
-    (instructions, nodes)
+struct Network<'input> {
+    instructions: Vec<Instruction>,
+    nodes: HashMap<&'input str, (&'input str, &'input str)>,
+}
+
+impl Network<'_> {
+    fn navigate<F>(&self, start_node: &str, is_end_node: F) -> usize
+    where
+        F: Fn(&str) -> bool,
+    {
+        let mut current_node = start_node;
+        for (i, instruction) in self.instructions.iter().cycle().enumerate() {
+            if is_end_node(current_node) {
+                return i;
+            }
+            let (lhs, rhs) = self.nodes[&current_node];
+            match instruction {
+                Instruction::Left => current_node = lhs,
+                Instruction::Right => current_node = rhs,
+            }
+        }
+
+        unreachable!()
+    }
+}
+
+impl<'input> From<&'input str> for Network<'input> {
+    fn from(s: &'input str) -> Network<'input> {
+        let mut lines = s.lines();
+        let instructions = lines
+            .next()
+            .unwrap()
+            .chars()
+            .map(Instruction::from)
+            .collect();
+        let nodes = lines
+            .skip(1)
+            .map(|line| {
+                let name = &line[0..3];
+                let lhs = &line[7..10];
+                let rhs = &line[12..15];
+                (name, (lhs, rhs))
+            })
+            .collect();
+        Network {
+            instructions,
+            nodes,
+        }
+    }
 }
 
 enum Instruction {
@@ -65,4 +94,5 @@ impl From<char> for Instruction {
 fn test() {
     assert_eq!(part1(include_str!("../examples/08_1.txt")), "2");
     assert_eq!(part1(include_str!("../examples/08_2.txt")), "6");
+    assert_eq!(part2(include_str!("../examples/08_3.txt")), "6");
 }
